8884191f03fd9f65705ddd2fadf088d794edc9d1
xl20071926 <lixin_1990@yeah.net>
2017-02-20 10:50:22 +0800

    add missing file

---
+++ LXPolygonView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXPolygonView
+//
+//  Created by Leexin on 17/2/5.
+//  Copyright © 2017年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
